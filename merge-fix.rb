1de6f1230cb03953ec30d174ddc4e4ad1cdf9b1d
rongi <rongi@users.noreply.github.com>
2016-05-12 16:40:02 +0200

    Update merge-fix.rb

--- merge-fix.rb
+++ merge-fix.rb
@@ -3,7 +3,7 @@ class MergeFix < Formula
   homepage "https://github.com/rongi/merge-fix"
   url "https://github.com/rongi/merge-fix/archive/1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "e9d14dda6784ea8d9c1f8d686ce0215889e22e4ff8ae67cb23d2647fb873f0e0"
+  sha256 "76c22375527ef9075818ca060d78f68d857a8a59fea6d4a5d65106aab922e969"
 
   def install
     bin.install "merge-fix"
