f2a920f7210095b0e910a3eebf829568953bb2de
rongi <rongi@users.noreply.github.com>
2016-06-07 16:16:53 +0200

    Update merge-fix.rb

--- merge-fix.rb
+++ merge-fix.rb
@@ -2,7 +2,7 @@ class MergeFix < Formula
   desc "Merges fix branch into develop branch"
   homepage "https://github.com/rongi/merge-fix"
   url "https://github.com/rongi/merge-fix/archive/1.0.0.tar.gz"
-  version "1.0.0"
+  version "1.0.1"
   sha256 "76c22375527ef9075818ca060d78f68d857a8a59fea6d4a5d65106aab922e969"
 
   def install
