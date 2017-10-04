a46db95761158589d102c328f943f92e73f54eff
Dmitry Ryadnenko <rongi@users.noreply.github.com>
2017-10-04 04:08:22 +0200

    Update dikter.rb

--- dikter.rb
+++ dikter.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Dikter < Formula
-  desc "Generates response boilerplate from json"
+  desc "Generates models and other boilerplate from json"
   homepage "https://github.com/rongi/dikter"
   url "https://github.com/rongi/dikter/releases/download/v0.1.0/dikter.tar"
   sha256 "1c1baa2d0401b5b7157fff0a0e9b32b3e63d87f6302c9b06db7781ac474f1f23"
