ee107b5d8a04b66a09b00a3ba45dce589b092ad9
Dmitry <dmitry@MacBook-Pro-5.lan>
2017-10-04 02:12:05 +0200

    dikter formula

---
+++ dikter.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Dikter < Formula
+  desc "Generates response boilerplate from json"
+  homepage "https://github.com/rongi/dikter"
+  url "https://github.com/rongi/dikter/releases/download/v0.1.0/dikter.tar"
+  sha256 "1c1baa2d0401b5b7157fff0a0e9b32b3e63d87f6302c9b06db7781ac474f1f23"
+
+  def install
+    bin.install "bin/dikter"
+  end
+end
