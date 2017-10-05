d6a7a0a92121b12b01638375d54c736b7c306124
Dmitry Ryadnenko <rongi@users.noreply.github.com>
2017-10-05 10:38:42 +0200

    Update dikter.rb

--- dikter.rb
+++ dikter.rb
@@ -6,6 +6,7 @@ class Dikter < Formula
   desc "Generates models and other boilerplate from json"
   homepage "https://github.com/rongi/dikter"
   url "https://github.com/rongi/dikter/releases/download/v0.1.1/dikter.tar"
+  # shasum -a 256 some_tarball.tar.gz 
   sha256 "bf9d6b63acf1339e294231447fe432ae666838c6866feaaefc8476d4789b59c5"
 
   def install
