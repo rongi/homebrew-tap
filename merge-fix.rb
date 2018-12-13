f8c3a92f51a5f86df2f0c6f07455f043c4f1933c
Dmitry Ryadnenko <rongi@users.noreply.github.com>
2018-12-13 18:53:54 +0100

    Update merge-fix.rb

--- merge-fix.rb
+++ merge-fix.rb
@@ -1,10 +1,10 @@
 class MergeFix < Formula
   desc "Merges fix branch into develop branch"
   homepage "https://github.com/rongi/merge-fix"
-  url "https://github.com/rongi/merge-fix/archive/v1.0.3.tar.gz"
-  version "1.0.3"
+  url "https://github.com/rongi/merge-fix/archive/v1.0.4.tar.gz"
+  version "1.0.4"
   # shasum -a 256 some_tarball.tar.gz 
-  sha256 "e00dd4d178567ac3509927ba55c290af76cedf0ad3db32232333a3d8cd790873"
+  sha256 "4881a5ccdb0babdefe7052d112527446f3b03bee90c14084b0e06aef0b675db5"
 
   def install
     bin.install "mergefix"
